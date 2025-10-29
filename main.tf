23ebf721c7c4ff4686eafc7257a062da64d230d5
SauravSameer <65173665+SauravSameer@users.noreply.github.com>
2025-10-29 12:13:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,19 @@
 provider "aws" {
-    region = "ap-south-1"
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "one" {
+  count = 4
+  ami = "ami-00af95fa354fdb788"
+  key_name = "terakey"
+  vpc_security_group_ids = ["sg-0c7ad9e34378a3399"]
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = var.instance_names[count.index]
   }
-  resource "aws_instance" "one" {
-   count = 4
-    ami = "ami-00af95fa354fdb788"
-    key name= "terakey"
-    vpc_security_group_ids= ["sg-0c7ad9e34378a3399"]
-    instance_type = "t3.micro"
-    tags = {
-      Name = var.instance_names[count.index]
-    }
-    }
+}
+
 variable "instance_names" {
-    default = ["jenkins", "tomcat-1", "tomcat-2", "monitoring server"]
+  default = ["jenkins", "tomcat-1", "tomcat-2", "monitoring-server"]
 }
