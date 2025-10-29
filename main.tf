3e5d6833bb0f493ee47fcabbb12dedc74a7531b6
SauravSameer <65173665+SauravSameer@users.noreply.github.com>
2025-10-29 12:10:40 +0530

    Modify AWS instance resource and add variable
    Updated AWS instance configuration and added variable for instance names.

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
 provider "aws" {
     region = "ap-south-1"
   }
-  resource "aws_instance" "naaaam" {
-   count = 3
-    ami = "ami-0f9708d1cd2cfee41"
+  resource "aws_instance" "one" {
+   count = 4
+    ami = "ami-00af95fa354fdb788"
+    key name= "terakey"
+    vpc_security_group_ids= ["sg-0c7ad9e34378a3399"]
     instance_type = "t3.micro"
     tags = {
-      Name = "devops"
+      Name = var.instance_names[count.index]
     }
     }
+variable "instance_names" {
+    default = ["jenkins", "tomcat-1", "tomcat-2", "monitoring server"]
+}
